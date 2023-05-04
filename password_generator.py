c044d778428d0a634543a73c2ceae2498b156404
pamidev <pamidev@duck.com>
2023-05-04 10:08:40 +0200

    Fixed typos and edited program messages

--- password_generator.py
+++ password_generator.py
@@ -2,27 +2,28 @@ import secrets
 import string
 
 
-def password_generator(pass_lenght:int = 8):
+def password_generator(pass_length: int = 12):
     characters_set = (string.ascii_letters +
                       string.digits +
                       string.punctuation)
     while True:
         gen_pass = ''
-        for l in range(pass_lenght):
+        for l in range(pass_length):
             gen_pass += ''.join(secrets.choice(characters_set))
         if (any(char in string.punctuation for char in gen_pass) and
-            sum(char in string.digits for char in gen_pass) >= 2 and
-            sum(char in string.ascii_lowercase for char in gen_pass) >= 2 and
-            sum(char in string.ascii_uppercase for char in gen_pass) >= 2):
+                sum(char in string.digits for char in gen_pass) >= 2 and
+                sum(char in string.ascii_lowercase for char in gen_pass) >= 2 and
+                sum(char in string.ascii_uppercase for char in gen_pass) >= 2):
             break
     return gen_pass
 
+
 try:
-    lenght = int(input("Enter password lenght (at least 8): "))
-    if lenght >= 8:
-        pwd = password_generator(lenght)
+    length = int(input("Enter password length (at least 12 characters): "))
+    if length >= 8:
+        pwd = password_generator(length)
         print(f"Your strong password is: {pwd}")
     else:
-        print("Check password lenght. Must be at least 8.")
+        print("Check password length. Must be at least 12 characters.")
 except ValueError:
     print("Entered bad data. Try again.")
