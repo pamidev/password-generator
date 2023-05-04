3cbf3eedb7695ed32a90b94eeb828c27d3107cc0
pamidev <pamidev@duck.com>
2023-05-04 10:22:13 +0200

    Refactored code by creating def main ang changing minimum of different characters in generated password

--- password_generator.py
+++ password_generator.py
@@ -11,19 +11,24 @@ def password_generator(pass_length: int = 12):
         for l in range(pass_length):
             gen_pass += ''.join(secrets.choice(characters_set))
         if (any(char in string.punctuation for char in gen_pass) and
-                sum(char in string.digits for char in gen_pass) >= 2 and
-                sum(char in string.ascii_lowercase for char in gen_pass) >= 2 and
-                sum(char in string.ascii_uppercase for char in gen_pass) >= 2):
+                sum(char in string.digits for char in gen_pass) >= 3 and
+                sum(char in string.ascii_lowercase for char in gen_pass) >= 3 and
+                sum(char in string.ascii_uppercase for char in gen_pass) >= 3):
             break
     return gen_pass
 
 
-try:
-    length = int(input("Enter password length (at least 12 characters): "))
-    if length >= 8:
-        pwd = password_generator(length)
-        print(f"Your strong password is: {pwd}")
-    else:
-        print("Check password length. Must be at least 12 characters.")
-except ValueError:
-    print("Entered bad data. Try again.")
+def main():
+    try:
+        length = int(input("Enter password length (at least 12 characters): "))
+        if length >= 8:
+            pwd = password_generator(length)
+            print(f"Your strong password is: {pwd}")
+        else:
+            print("Check password length. Must be at least 12 characters.")
+    except ValueError:
+        print("Entered bad data. Try again.")
+
+
+if __name__ == "__main__":
+    main()
